06fb76d16bb10d1bc003094f3ce479200dae258d
Orrin Rohrer <orrin.rohrer@gmail.com>
2023-09-15 15:26:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.14.0"
+      version = "~> 5.0"
     }
   }
-}
-provider "aws" {
-  region = var.aws_region
 }
